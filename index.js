84546b058a84471ff8fbeed15151ee1020e66b9a
DarrenHDixon <53707335+DarrenHDixon@users.noreply.github.com>
2019-11-07 16:40:52 +0000

    Added files for Dice Game

---
+++ index.js
@@ -0,0 +1,29 @@
+var randomNumber1 = Math.floor(Math.random() * 6); //roll 1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";//dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //change source attribute of the image; images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+//PLAYER 2
+var randomNumber2 = Math.floor(Math.random() * 6); //roll 1-6
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+//Player 1 Wins
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+//Player 2 wins
+else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+//It's a draw
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
